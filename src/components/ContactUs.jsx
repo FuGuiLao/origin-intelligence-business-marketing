8d3173be4c6e1c2d2fe8fa3d7a58c78266205c53
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2024-04-15 21:11:45 -0700

    Update ContactUs.jsx

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,8 +1,34 @@
 const ContactUs = () => {
   return (
-    <iframe src='
-https://outlook.office365.com/owa/calendar/ORIGINClientConsultation@origininvestigations.com/bookings/'
-width='100%' height='100%' scrolling='yes' style='border:0'></iframe>
+   <div className="typography text-center">
+      <div className="w-fit mx-auto">
+        <h2 className="w-fit uppercase !mt-4">Los Angeles</h2>
+        <div className="w-full h-[1px] bg-[#111827] dark:bg-white !mt-2" />
+      </div>
+      <p className="!mt-4">
+        515 S. Flower Street 18th Floor<br />
+        Los Angeles, CA 90071<br />
+        (213) 314-5229
+      </p>
+      <div className="w-fit mx-auto">
+        <h2 className="w-fit uppercase !mt-4">New York</h2>
+        <div className="w-full h-[1px] bg-[#111827] dark:bg-white !mt-2" />
+      </div>
+      <p className="!mt-4">
+        287 Park Avenue South 2nd Floor<br />
+        New York, NY 10010<br />
+        (212) 860-0098
+      </p>
+      <div className="w-fit mx-auto">
+        <h2 className="w-fit uppercase !mt-4">Washington DC</h2>
+        <div className="w-full h-[1px] bg-[#111827] dark:bg-white !mt-2" />
+      </div>
+      <p className="!mt-4">
+        1441 L Street NW 10th Floor<br />
+        Washington, DC 20005<br />
+        (202) 558-4090
+      </p>
+    </div>
   )
 }
 
