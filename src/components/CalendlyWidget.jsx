080f86e6c41db39a9307fd44a2a079bf53417b8b
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2024-12-30 00:40:58 -0800

    Update CalendlyWidget.jsx

--- src/components/CalendlyWidget.jsx
+++ src/components/CalendlyWidget.jsx
@@ -12,7 +12,7 @@ function CalendlyWidget() {
   }, []);
 
   return (
-    <div className="calendly-inline-widget" data-url={`https://calendly.com/origin-intelligence/client-consultation-web/?hide_landing_page_details=1&hide_gdpr_banner=1${resolvedTheme === 'dark' ? "&primary_color=ffffff4d&text_color=ffffff4d&background_color=030712" : "&primary_color=111827&text_color=111872&background_color=ffffff"}`} style={{ minWidth: '320px', height: '100%' }} />
+    <div className="calendly-inline-widget" data-url={`https://calendly.com/origin-intelligence/client-consultation-web/?hide_landing_page_details=1&hide_gdpr_banner=1${resolvedTheme === 'dark' ? "&primary_color=ffffff&text_color=ffffff&background_color=030712" : "&primary_color=111827&text_color=111827&background_color=ffffff"}`} style={{ minWidth: '320px', height: '100%' }} />
   );
 }
 
